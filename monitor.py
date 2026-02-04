d371ed938ae60bd4324ea7ba8e1d182dc4610997
dametest-git <dametest@outlook.com>
2026-02-04 09:23:14 +0100

    Refactor monitor.py to improve structure and functionality
    Refactor monitor script to use Monitor class and simplify periodic checks.

--- monitor.py
+++ monitor.py
@@ -1,31 +1,20 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import xbmc
-import threading
 import time
-import xbmcaddon
+from service import do_upload
 
-from service import log, upload_files
+class Monitor(xbmc.Monitor):
+    def __init__(self):
+        self.check_interval = 3 * 3600  # 3h v sekundách
 
-ADDON = xbmcaddon.Addon('script.dame_ftp_upload')
+    def onInit(self):
+        if ADDON.getSetting('start_on_boot') == 'true':
+            xbmc.executebuiltin('RunScript(script.dame_ftp_upload)')
 
-def periodic_check():
-    while not xbmc.abortRequested():
-        if ADDON.getSetting("check_regularly") == "true":
-            freq = float(ADDON.getSetting("check_frequency")) * 3600
-            upload_files()
-            time.sleep(max(freq, 3600))  # Min 1h
-        else:
-            time.sleep(1800)  # Check settings 30min
+monitor = Monitor()
 
-if __name__ == '__main__':
-    log("Service spustený")
-    if ADDON.getSetting("start_on_boot") == "true":
-        upload_files()
-    
-    check_thread = threading.Thread(target=periodic_check, daemon=True)
-    check_thread.start()
-    
-    while not xbmc.abortRequested():
-        xbmc.sleep(1000)
-    log("Service ukončený")
+while not xbmc.abortRequested():
+    if ADDON.getSetting('check_regularly') == 'true':
+        do_upload()
+        time.sleep(int(ADDON.getSetting('check_frequency')) * 3600)
+    if monitor.waitForAbort(1):
+        break
